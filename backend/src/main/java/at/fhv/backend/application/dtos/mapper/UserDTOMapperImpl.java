06d0fd225c55b6e077788305bb720efcae0708e2
Emanuel Peter <157378848+Ema645@users.noreply.github.com>
2026-04-08 22:55:17 +0200

    add: implement UserDTOMapper and UserDTOMapperImpl for user response mapping

---
+++ backend/src/main/java/at/fhv/backend/application/dtos/mapper/UserDTOMapperImpl.java
@@ -0,0 +1,13 @@
+package at.fhv.backend.application.dtos.mapper;
+
+import at.fhv.backend.application.dtos.response.UserResponseDTO;
+import at.fhv.backend.domain.model.user.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDTOMapperImpl implements UserDTOMapper {
+    @Override
+    public UserResponseDTO toResponseDTO(User user) {
+        return new UserResponseDTO(user.getId(), user.getUsername());
+    }
+}
